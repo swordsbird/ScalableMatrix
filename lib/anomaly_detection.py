062babf5ded31786b6fa964abb8eee63efbd1cdb
swordbird <thu.lz@outlook.com>
2022-07-02 23:14:09 +0800

    update

--- lib/anomaly_detection.py
+++ lib/anomaly_detection.py
@@ -9,6 +9,157 @@ from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 from sklearn.linear_model import LinearRegression
 
+
+from sklearn.metrics import pairwise_distances
+
+import numpy as np
+import math
+
+from typing import Union
+
+
+def run_loci(data: np.ndarray, alpha: float = 0.5, k: int = 3):
+    """Run the LOCI algorithm on the specified dataset.
+    Runs the LOCI algorithm for the specified datset with the specified
+    parameters, returns a LOCI object, from which outlier indices can
+    be accessed via the indice property.
+    Parameters
+    ----------
+    data: np.ndarray
+        Shape - [number of datapoints, number of dimensions]
+    alpha: float, optional
+        Default is 0.5 as per the paper. See the paper for full details.
+    k: int, optional
+        Default is 3 as per the paper. See the paper for full details.
+    """
+    loci_i = LOCIMatrix(data, alpha, k)
+    loci_i.run()
+    return loci_i
+
+
+
+class LOCIMatrix():
+    """
+    data: np.ndarray
+    alpha: float, optional
+    k: int, optional
+    See the loci function for more details on the parameters.
+    Attributes
+    ----------
+    _data: np.ndarray
+    _alpha: float
+    _k: int
+    max_dist: float
+    n_points: int
+    indice: np.ndarray
+    _dist_matrix: np.ndarray
+        The distance matrix, has shape [n_data_points, n_data_points]
+    """
+
+    def __init__(self, data: np.ndarray, alpha: float = 0.85, k: int = 3):
+        self.data = data
+        self.alpha = alpha
+        self.k = k
+
+        self.max_dist = None
+        self.n_points = self.data.shape[0]
+        self.indice = None
+        self.dist_matrix = pairwise_distances(X = self.data, metric='cosine')
+        self.sorted_neighbors = np.argsort(self.dist_matrix, axis=1)
+        self.sorted_dist = np.sort(self.dist_matrix, axis=1)
+        self.outer_ptr = np.zeros(self.n_points).astype(int)
+        self.inner_ptr = np.zeros(self.n_points).astype(int)
+
+    def update_outer_pointer(self, r):
+        ptr = self.outer_ptr
+        for i in range(self.n_points):
+            while ptr[i] < self.n_points and self.sorted_dist[i, ptr[i]] <= r:
+                ptr[i] += 1
+
+    def update_inner_pointer(self, r):
+        ptr = self.inner_ptr
+        for i in range(self.n_points):
+            while ptr[i] < self.n_points and self.sorted_dist[i, ptr[i]] <= r:
+                ptr[i] += 1
+
+    def run(self):
+        """Executes the LOCI algorithm"""
+        self.result = []
+        for p_ix in range(self.n_points):
+            self.result.append({
+                'records': [],
+            })
+        sqrt_n = int(math.sqrt(self.n_points))
+        self.alpha = self.sorted_dist[:, 20].mean() / self.sorted_dist[:, int(math.sqrt(self.n_points))].mean()
+        r_max = self.sorted_dist[:, sqrt_n].max() / self.alpha
+        r_min = self.sorted_dist[:, 10].min()
+        self.rs = []
+        self.scores = [[] for _ in range(self.n_points)]
+        print('r range: %.3f - %.3f, alpha: %.3f' % (r_min, r_max, self.alpha))
+
+        n_steps = 250
+        for i in range(n_steps):
+            r = i / n_steps * (r_max - r_min) + r_min
+            self.rs.append(r)
+            self.update_outer_pointer(r)
+            self.update_inner_pointer(self.alpha * r)
+            for p_ix in range(self.n_points):
+                neighbors = self._get_sampling_N(p_ix)
+                n_values = self._get_alpha_n(neighbors)
+                cur_alpha_n = self._get_alpha_n(p_ix)
+
+                n_hat = np.mean(n_values)
+                mdef = 1 - (cur_alpha_n / n_hat)
+                sigma_mdef = np.std(n_values) / n_hat
+                self.result[p_ix]['records'].append((r, mdef, sigma_mdef))
+
+                score = 0
+                if len(neighbors) >= 20:
+                    score = mdef / sigma_mdef
+                self.scores[p_ix].append(score)
+
+        step = (r_max - r_min) / n_steps
+        r = self.sorted_dist[:, int(math.sqrt(self.n_points))].mean()
+        self.indice = np.array([int((self.sorted_dist[i, sqrt_n] - r_min) / step) for i in range(self.n_points)])
+        self.min_indice = np.array([self.scores[i].argmin() for i in range(self.n_points)])
+        self.max_indice = np.array([self.scores[i].argmax() for i in range(self.n_points)])
+        self.outlier_score = np.array([self.scores[i, self.indice[i]] for i in range(self.n_points)])
+        self.scores = np.array(self.scores)
+        return True
+
+    def label_propagation(self, x, label, thres = 0.10):
+        conf = {}
+        visit = {}
+        conf[x] = 1
+        Q = [x]
+        head = 0
+        while head < len(Q):
+            x = Q[head]
+            head += 1
+            if x in visit:
+                continue
+            visit[x] = 1
+            
+            if label == 0:
+                new_indice = self.min_indice[x]
+            else:
+                new_indice = self.max_indice[x]
+            self.indice[x] = self.indice[x] * conf[x] + new_indice * (1 - conf[x])
+            for i, j in enumerate(self.sorted_neighbors[x]):
+                sim = (1 - self.sorted_dist[x, i]) * conf[x]
+                if sim < thres:
+                    break
+                conf[j] = conf.get(j, 0) + sim
+                Q.append(j)
+
+    def _get_sampling_N(self, p_ix: int):
+        return self.sorted_neighbors[p_ix][:self.outer_ptr[p_ix]]
+
+    def _get_alpha_n(self, indices: Union[int, np.ndarray]):
+        return self.inner_ptr[indices]
+
+
+
 class DetectorEnsemble:
     def __init__(self, mode = 'iforest', adjust_sample_weight = 0.01):
         self.detectors = []
